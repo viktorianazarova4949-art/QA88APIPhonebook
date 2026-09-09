052ac76221ebbdf43279825daf41cc03b663388e
vikto <viktoria.nazarova4949@gmail.com>
2026-09-09 15:36:14 +0300

    Start project

--- utils/conftest.py
+++ utils/conftest.py
@@ -1,8 +1,14 @@
-
+import random
+import time
+import password
 import pytest
 import requests
-from config import *
+from faker import Faker
 
+from config import *
+from models.user_dto import User
+
+fake = Faker()
 
 @pytest.fixture(scope="session")
 def registration_url():
@@ -17,5 +23,14 @@ def session_id():
     s=requests.Session()
     yield s  # kanta testista ja kini
     s.close()
-
-
+@pytest.fixture(scope="function")
+def random_user():
+    username = f"qa_{int(time.time())}_{fake.email()}"
+    password = fake.password(
+        length=random.randint(8, 15),
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    )
+    return User(username=username, password=password)
