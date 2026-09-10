05265aace56f42a2d4179e7f71673909b7ed5941
vikto <viktoria.nazarova4949@gmail.com>
2026-09-10 15:41:00 +0300

    RegestrationTests

--- test/test_registation.py
+++ test/test_registation.py
@@ -1,3 +1,5 @@
+import pytest
+
 from conftest import *
 
 
@@ -29,3 +31,28 @@ class TestRegistration:
         print(response.json())
         assert response.status_code in [400,409]
         assert "User already exists" in response.json().values()
+
+    @pytest.mark.parametrize("invalid_email",[
+        "vbgysh123.ghh.ghu",
+        "ffuj45@",
+        "@gmail.com",
+        "nbjkl654@gmail",
+        "efegrg455@@fghj.hjk",
+        "et85 @fff.fd"
+])
+
+    def test_registration_negative_invalid_email(self, session, registration_url, invalid_email):
+        user = User(invalid_email, "Qwerty123$")
+        body = {
+            "username": user.username,
+            "password": user.password,
+        }
+        headers = {
+            "Content-Type": "application/json",
+        }
+        session.post(registration_url, json=body, headers=headers)
+        response = session.post(registration_url, json=body, headers=headers)
+        print(response.json())
+        assert response.status_code==400  # Bug
+
+
